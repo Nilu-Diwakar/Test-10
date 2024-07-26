481464f1bf2459569a630bc558c53cb98ce058e5
Nilu-Diwakar <nilu000diwakar@gmail.com>
2024-07-26 19:24:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,18 +128,16 @@ async def main(bot: Client, message: Message):
                                      disable_web_page_preview=True)
             return
 
-        if Config.OTHER_USERS_CAN_SAVE_FILE is False:
-            return
-
-        await message.reply_text(
-            text="**Choose an option from below:**",
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("Save in Batch", callback_data="addToBatchTrue")],
-                [InlineKeyboardButton("Get Sharable Link", callback_data="addToBatchFalse")]
-            ]),
-            quote=True,
-            disable_web_page_preview=True
-        )
+        if filters.user(BOT_OWNER):
+            await message.reply_text(
+                text="**Choose an option from below:**",
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton("Save in Batch", callback_data="addToBatchTrue")],
+                    [InlineKeyboardButton("Get Sharable Link", callback_data="addToBatchFalse")]
+                ]),
+                quote=True,
+                disable_web_page_preview=True
+            )
     elif message.chat.type == enums.ChatType.CHANNEL:
         if (message.chat.id == int(Config.LOG_CHANNEL)) or (message.chat.id == int(Config.UPDATES_CHANNEL)) or message.forward_from_chat or message.forward_from:
             return

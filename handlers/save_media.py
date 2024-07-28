a9b444e2dcd89471c6c210e516ebc0ca642d17a1
Nilu-Diwakar <nilu000diwakar@gmail.com>
2024-07-29 05:25:37 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -61,8 +61,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             await asyncio.sleep(2)
         SaveMessage = await bot.send_message(
             chat_id=Config.DB_CHANNEL,
-            text=f"**Original Link** = <code>{short_link}</code> \n\n",
-            f"**Short Link** = <code>{share_link}</code> \n\n", message_ids_str,
+            text=message_ids_str,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
